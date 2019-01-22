d6975b8d1204a8bfafc54707245859cefb069bc4
cg1101 <cg1101@gmail.com>
2019-01-22 22:02:25 +1100

    update email in issuer.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "building docker images ... "
+
 docker build -t "${DOCKER_ID}/multi-client:latest" \
              -t "${DOCKER_ID}/multi-client:${SHA}" \
              -f ./client/Dockerfile ./client
@@ -10,6 +12,7 @@ docker build -t "${DOCKER_ID}/multi-worker:latest" \
              -t "${DOCKER_ID}/multi-worker:${SHA}" \
              -f ./worker/Dockerfile ./worker
 
+echo "publishing docker images to docker hub ... "
 docker push "${DOCKER_ID}/multi-client:latest"
 docker push "${DOCKER_ID}/multi-client:${SHA}"
 docker push "${DOCKER_ID}/multi-server:latest"
@@ -17,8 +20,12 @@ docker push "${DOCKER_ID}/multi-server:${SHA}"
 docker push "${DOCKER_ID}/multi-worker:latest"
 docker push "${DOCKER_ID}/multi-worker:${SHA}"
 
+echo "update kubernetes ... "
 kubectl apply -f k8s
 
+echo "set docker image imperatively .... "
 kubectl set image deployments/server-deploy server={$DOCKER_ID}/multi-server:${SHA}
 kubectl set image deployments/client-deploy client={$DOCKER_ID}/multi-client:${SHA}
 kubectl set image deployments/worker-deploy worker={$DOCKER_ID}/multi-worker:${SHA}
+
+echo "deployment is done"
